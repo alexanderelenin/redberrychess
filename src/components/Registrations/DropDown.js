7d86f556e5bcc9a4cd528cd2d7898bef23ef8a4e
alexanderelenin <elenin.workspace@gmail.com>
2022-07-06 22:45:15 +0400

    dropdown added

---
+++ src/components/Registrations/DropDown.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import chevrondown from "../../assets/chevron.png";
+import chevronup from "../../assets/chevronup.png";
+
+const DropDown = ({ selected, setSelected }) => {
+  const [isActive, setIsActive] = useState(false);
+  const options = ["Beginner", "Intermediate", "Professional"];
+  return (
+    <div className="dropdown">
+      <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
+        <p>{selected}</p>
+        <img src={!isActive ? chevrondown : chevronup} />
+      </div>
+      {isActive && (
+        <div className="dropdown-content">
+          {options.map((option) => (
+            <div
+              onClick={(e) => {
+                setSelected(option);
+                setIsActive(!isActive);
+              }}
+              className="dropdown-item"
+              key={options[option]}
+            >
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropDown;
